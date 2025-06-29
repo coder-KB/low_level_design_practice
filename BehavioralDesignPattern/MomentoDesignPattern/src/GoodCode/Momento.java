6add7b6d9ccb4046c9a53f6c6bf9c710e9865086
Bharath Choudhary <coderkb@gmail.com>
2025-06-29 19:51:50 +0530

    Implemented Momento Design Pattern Code

---
+++ BehavioralDesignPattern/MomentoDesignPattern/src/GoodCode/Momento.java
@@ -0,0 +1,13 @@
+package MomentoDesignPattern.src.GoodCode;
+
+public class Momento {
+    private final String text;
+
+    public Momento(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
